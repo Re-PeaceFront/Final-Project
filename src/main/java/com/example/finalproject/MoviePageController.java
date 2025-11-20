07a7c8dfe53d5c10b1f8d207a2ae4634e2f577ea
aya <nellymtl4@gmail.com>
2025-11-20 15:34:00 -0500

    Add the MoviePageController

---
+++ src/main/java/com/example/finalproject/MoviePageController.java
@@ -0,0 +1,9 @@
+package com.example.finalproject;
+
+import javafx.event.ActionEvent;
+
+public class MoviePageController {
+    public void onTicketsButtonClick(ActionEvent actionEvent) {
+
+    }
+}
