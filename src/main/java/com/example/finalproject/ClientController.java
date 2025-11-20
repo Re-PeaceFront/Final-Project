8df47eca40b82a4ae757178ba1fad3179341ba7b
aya <nellymtl4@gmail.com>
2025-11-20 09:26:05 -0500

    Code the sign up button 2

--- src/main/java/com/example/finalproject/ClientController.java
+++ src/main/java/com/example/finalproject/ClientController.java
@@ -1,9 +1,28 @@
 package com.example.finalproject;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class  ClientController {
-    public void onSignupButtonClick(ActionEvent actionEvent) {
-        
+import java.io.IOException;
+
+public class ClientController {
+
+    @FXML
+    public void onSignupButtonClick(ActionEvent actionEvent) throws IOException {
+        // Load the SignUp page
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("client-view.fxml"));
+        Parent root = loader.load();
+        // Get current stage
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        // Set new scene
+        stage.setScene(new Scene(root));
+        stage.setTitle("Sign Up Page ");
+        stage.show();
     }
 }
+
